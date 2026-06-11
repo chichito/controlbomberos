6e0be67f6748214dae2cf3e80a56c8c0b473ac15
Fernando Cando Chacon <fernandocando@hotmail.com>
2026-06-11 11:31:53 -0500

    primerprueba

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    /*
+    /* bine
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       navigatorKey: navigatorKey,
